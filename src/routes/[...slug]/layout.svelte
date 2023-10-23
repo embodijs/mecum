5d0cafdab0bf35b03a747193781cacdb16afc210
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2023-10-23 21:12:50 +0200

    add +layout.svelte to import from template

---
+++ src/routes/[...slug]/layout.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+    import TemplateLayout from "$__embodi/template/layout";
+</script>
+
+<script>
+    export * from "$__embodi/template/layout";
+</script>
+
+<TemplateLayout {...$$restProps} />
